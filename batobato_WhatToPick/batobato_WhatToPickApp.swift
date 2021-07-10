00f42849dcf44ce2e42ada7705a098331e14e364
Jobie Gamao <JobieGamao@jobiemaygamao@gmail.com>
2021-07-10 15:52:33 +0800

    Initial Commit

---
+++ batobato_WhatToPick/batobato_WhatToPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  batobato_WhatToPickApp.swift
+//  batobato_WhatToPick
+//
+//  Created by Jobie May Gamao on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct batobato_WhatToPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
